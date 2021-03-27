c2b36447426c4c2445a0db287b4d19ea4fdf8ee9
AabhasAgrawal <73350120+AabhasAgrawal@users.noreply.github.com>
2021-03-27 13:27:45 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,28 @@
+/*class Form {
+    constructor() {
+      
+    
+     
+    }
+  
+ 
+    display(){
+    
+      this.input.position(130, 160);
+      this.button.position(250, 200);
+      this.input
+  
+      this.button.mousePressed(()=>{
+
+        this.name = this.input.value();
+        
+        player.name = this.input.value();
+        playerCount+=1;
+        player.index = playerCount;
+        player.update();
+        player.updateCount(playerCount);
+      
+      });
+    }
+  }*/
+  
---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+
+function setup (){
+
+
+    input2 = createInput("Enter Correct Option No.."); 
+    this.button = createButton('Submit');
+    input1 = createInput("Enter Your Name Here...."); 
+
+}
